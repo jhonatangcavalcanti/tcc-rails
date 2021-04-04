652f028e6c12355766a33a5441982f431c7dd1d9
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2021-04-04 11:16:44 -0300

    destroy dependents on destroy

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,6 @@ class Room < ApplicationRecord
   belongs_to :building
   belongs_to :room_type
 
-  has_many :allocations
+  has_many :allocations, dependent: :destroy
   has_many :employees, through: :allocations
 end
