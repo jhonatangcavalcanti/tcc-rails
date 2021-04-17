30835ae7a0fb354c719f6910cd284d4708874de1
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2021-04-17 19:42:43 -0300

    add semantic title to allocation

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -2,4 +2,8 @@ class Allocation < ApplicationRecord
   belongs_to :employee
   belongs_to :room
   belongs_to :allocation_role
+
+  def title
+    self.employee && self.allocation_role && self.room ? "#{self.employee.name} - #{self.allocation_role.name} - #{self.room.name}" : ""
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,6 @@
 class Employee < ApplicationRecord
   has_and_belongs_to_many :job_position
+
   has_many :allocations, dependent: :destroy
   has_many :rooms, through: :allocations
 end
