fd61900858f7afe92eb0c0a518890d71a58a1891
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2021-10-21 01:27:21 -0300

    set props to improve multiselect widget for allocation role

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -1,7 +1,7 @@
 class Allocation < ApplicationRecord
   belongs_to :employee, inverse_of: :allocations
   belongs_to :room, inverse_of: :allocations
-  belongs_to :allocation_role
+  belongs_to :allocation_role, inverse_of: :allocations
 
   def title
     self.employee && self.allocation_role && self.room ? "#{self.employee.name} - #{self.allocation_role.name} - #{self.room.name}" : ""
--- app/models/allocation_role.rb
+++ app/models/allocation_role.rb
@@ -1,3 +1,4 @@
 class AllocationRole < ApplicationRecord
-  has_many :allocations
+  has_many :allocations, inverse_of: :allocation_role
+  accepts_nested_attributes_for :allocations, allow_destroy: true
 end
