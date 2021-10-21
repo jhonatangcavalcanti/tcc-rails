212fc10f3eecbe346478e8e4008846689e329635
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2021-10-21 00:21:01 -0300

    add title to position model

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,8 @@
 class Position < ApplicationRecord
   belongs_to :employee
   belongs_to :job_position
+
+  def title
+    self.employee && self.job_position ? "#{self.employee.name} - #{self.job_position.name}" : ""
+  end
 end
