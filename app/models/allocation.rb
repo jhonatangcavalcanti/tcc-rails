39211c07a56f7c2a85c24e372f20e934ab0e9cda
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2021-10-21 23:07:41 -0300

    set number as title for room

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -4,6 +4,6 @@ class Allocation < ApplicationRecord
   belongs_to :allocation_role, inverse_of: :allocations
 
   def title
-    self.employee && self.allocation_role && self.room ? "#{self.employee.name} - #{self.allocation_role.name} - #{self.room.name}" : ""
+    self.employee && self.allocation_role && self.room ? "#{self.employee.name} - #{self.allocation_role.name} - #{self.room.number}" : ""
   end
 end
