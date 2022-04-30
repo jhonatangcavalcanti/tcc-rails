f191b1b5cd6df83ea55c7175f0332381175e8110
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2022-04-30 01:10:27 -0300

    update rails admin config

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,4 +5,15 @@ class Employee < ApplicationRecord
   has_many :positions, inverse_of: :employee
   has_many :job_positions, through: :positions
   accepts_nested_attributes_for :positions, allow_destroy: true
+
+  rails_admin do
+    configure :positions do
+      visible(false)
+    end
+
+    configure :job_positions do
+      orderable(true) # only for multiselect widget currently. Will add the possibility to order blocks
+      # configuration here
+    end
+  end
 end
