01cd225198e67d7016dc45d9e3f13a6195ed8945
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2022-05-01 18:46:20 -0300

    update login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   enum kind: [:editor, :admin]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :validatable
+  # :rememberable, :registerable, :recoverable
 
   def admin?
     return self.kind == 'admin'
